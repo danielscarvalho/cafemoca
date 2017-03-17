79c51be0be968a98e3c3d597bac213ce8e6f3480
Daniel Carvalho <danielscarvalho@gmail.com>
2017-03-17 14:05:17 -0300

    init cafe moca app

---
+++ cafemoca.py
@@ -0,0 +1,12 @@
+# App: cafemoca
+# Author: @danielscarvalho
+# Date: Feb/2016
+# Code: main app
+
+import cmlib
+
+print("cafemoca app")
+x1, x2 = cmlib.bhaskara(2, 6, 4)
+print("x\'  = {}\nx'' = {}".format(x1, x2))
+
+
---
+++ cmlib.py
@@ -0,0 +1,12 @@
+# App: cafemoca
+# Author: @danielscarvalho
+# Date: Feb/2016
+# Code: cf lib
+import math
+ 
+def bhaskara(a, b, c):
+    x=math.sqrt((b**2)-(4*a*c))
+    x1=(-b+x)/(2*a)
+    x2=(-b-x)/(2*a)
+    return x1, x2
+
