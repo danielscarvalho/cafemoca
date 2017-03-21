64b443be31a45bd0ab37b414108eb6426100660c
Fitoplancton <fitoplancton@live.com>
2017-03-20 23:30:45 -0300

    comments - documentation

--- cmlib.py
+++ cmlib.py
@@ -4,18 +4,21 @@
 # Code: cf lib
 
 import math
- 
+
+# bhaskara calculation: http://mathworld.wolfram.com/QuadraticEquation.html 
 def bhaskara(a, b, c):
     x=math.sqrt((b**2)-(4*a*c))
     x1=(-b+x)/(2*a)
     x2=(-b-x)/(2*a)
     return x1, x2
 
+# Statistics: Average: http://mathworld.wolfram.com/Mean.html 
 def average(values):
     vsum = sum(values)
     itens = len(values)
     return vsum / float(itens)
 
+# Statistics: Variance: http://mathworld.wolfram.com/Variance.html
 def variance(values):
     vaverage = average(values)
     vsum = 0
@@ -28,5 +31,8 @@ def variance(values):
     
     return vvariance
 
+# Statistics: Standard Deviation: http://mathworld.wolfram.com/StandardDeviation.html:
 def standard_deviation(values):
     return math.sqrt(variance(values))
+
+
